bdcc1d5253c4ee14107abf1eb89d389fd803b751
Bakhyt <b-root@list.ru>
2020-04-22 01:31:37 +0600

    new func

--- sandbox/src/main/java/org/baha/MyFirstProgramm.java
+++ sandbox/src/main/java/org/baha/MyFirstProgramm.java
@@ -2,9 +2,24 @@ package org.baha;
 
 public class MyFirstProgramm {
 
-  public static void main(String[] args) {
-    int l = 8;
-    System.out.println("Super ad");
-    System.out.println( l + "=" + (l * l));
-  }
+    public static void main(String[] args) {
+        hello("world");
+        double g = 6;
+        System.out.println(g + "=" + area(g));
+
+        double a = 12;
+        double b = 3;
+        System.out.println( a + b + "=" + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("hello world");
+    }
+
+    public static double area(double g) {
+        return g * g;
+    }
+    public  static double  area(double a,double b){
+        return a / b;
+    }
 }
