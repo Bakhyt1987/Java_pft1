4ad513e42c80b69411076f9facd751eac18bf9eb
Bakhyt <b-root@list.ru>
2020-04-24 04:17:26 +0600

    refactor extract parameter/method

---
+++ addressbook-web-tests/src/test/java/org/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package org.addressbook;
+
+public class GroupData {
+    private final String name;
+    private final String groupHeader;
+    private final String groupFooter;
+
+    public GroupData(String name, String groupHeader, String groupFooter) {
+        this.name = name;
+        this.groupHeader = groupHeader;
+        this.groupFooter = groupFooter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroupHeader() {
+        return groupHeader;
+    }
+
+    public String getGroupFooter() {
+        return groupFooter;
+    }
+}
