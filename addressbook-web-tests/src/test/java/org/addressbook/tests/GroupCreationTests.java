eeae4e12b2432d67ed0093ea65d4c3d84b2b787e
Bakhyt Zhapparbergenov <B-root@list.ru>
2020-05-10 16:51:06 +0600

    extensions

--- addressbook-web-tests/src/test/java/org/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/org/addressbook/tests/GroupCreationTests.java
@@ -1,26 +1,26 @@
 package org.addressbook.tests;
 
 import org.addressbook.modules.GroupData;
-import org.testng.Assert;
+import org.addressbook.modules.Groups;
 import org.testng.annotations.Test;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
 
     @Test
-    public void firstTest() {
+    public void testGroupCreation() {
         app.goTo().groupPage();
-        Set<GroupData> before = app.group().all();
-        GroupData group = new GroupData().withName("test2").withHeader("test3").withFooter("test4");
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("test1").withHeader("test3").withFooter("test4");
         app.group().creat(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-        before.add(group);
-        Assert.assertEquals(before.size(), after.size());
 
     }
 
